e26879e9369021669f4b80db046e16627940e6dc
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-06-05 11:45:45 +0100

    refactor: Use intermediate variables to improve readability and convey meaning

--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -2,13 +2,19 @@ const { humanCatDogYears } = require("../src");
 
 describe("humanCatDogYears", () => {
   it("should take an amount of time in human years and return an object containing the number of years in dog years and cat years", () => {
-    expect(humanCatDogYears(1)).toStrictEqual({
-      dogYears: 15,
-      catYears: 15
-    });
-    expect(humanCatDogYears(2)).toStrictEqual({
-      dogYears: 24,
-      catYears: 24
+    const humanYearsA = 1;
+    const dogCatYearsA = { dogYears: 15, catYears: 15 };
+    expect(humanCatDogYears(humanYearsA)).toStrictEqual(dogCatYearsA);
+    const humanYearsB = 2;
+    const dogCatYearsB = { dogYears: 24, catYears: 24 };
+    expect(humanCatDogYears(humanYearsB)).toStrictEqual(dogCatYearsB);
+    const humanYearsC = 3;
+    const dogCatYearsC = { dogYears: 29, catYears: 28 };
+    expect(humanCatDogYears(humanYearsC)).toStrictEqual(dogCatYearsC);
+    const humanYearsD = 4;
+    const dogCatYearsD = { dogYears: 34, catYears: 32 };
+    expect(humanCatDogYears(humanYearsD)).toStrictEqual(dogCatYearsD);
+  });
     });
     expect(humanCatDogYears(3)).toStrictEqual({
       dogYears: 29,
